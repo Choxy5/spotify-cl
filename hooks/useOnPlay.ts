3ae5296e67f13e64e2534d5abc63c8e77654eb21
Choxy5 <kristijan.blazevic000@gmail.com>
2023-06-13 19:50:52 +0200

    Player Functionality

---
+++ hooks/useOnPlay.ts
@@ -0,0 +1,23 @@
+import useAuthModal from '@/hooks/useAuthModal';
+import usePlayer from '@/hooks/usePlayer';
+import { useUser } from '@/hooks/useUser';
+import { Song } from '@/types';
+
+const useOnPlay = (songs: Song[]) => {
+  const player = usePlayer();
+  const authModal = useAuthModal();
+  const { user } = useUser();
+
+  const onPlay = (id: string) => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    player.setId(id);
+    player.setIds(songs.map((song) => song.id));
+  };
+
+  return onPlay;
+};
+
+export default useOnPlay;
